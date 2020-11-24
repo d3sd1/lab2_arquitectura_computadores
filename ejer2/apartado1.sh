a179ab97b85e7508a27309bfbc1eff87e3086422
Andrei García <andreigarciacuadra@gmail.com>
2020-11-24 18:16:34 +0100

    second release holafefirño

--- ejer2/apartado1.sh
+++ ejer2/apartado1.sh
@@ -15,12 +15,15 @@ valgrind --tool=cachegrind ./access_seq --L1=32768,8,64 --L2=262144,8,64
 cp cachegrind.out.* out_3.out
 rm -rf ./cachegrind.out.*
 
+cat *.out >> out_global.out
 cg_annotate out_1.out --auto=yes &> ../results/ejer2/ap1_1.result
 cg_annotate out_2.out --auto=yes &> ../results/ejer2/ap1_2.result
 cg_annotate out_3.out --auto=yes &> ../results/ejer2/ap1_3.result
+cg_annotate out_global.out --auto=yes &> ../results/ejer2/ap1_global.result
 cp out_1.out ../results/ejer2/ap1_1.out_valgrind
 cp out_2.out ../results/ejer2/ap1_2.out_valgrind
 cp out_3.out ../results/ejer2/ap1_3.out_valgrind
+cp out_global.out ../results/ejer2/ap1_global.out_valgrind
 
 echo "Observe los resultados obtenidos e inspeccione el código con la herramienta cg_annotate. Anote
 los resultados globales y observe los resultados prestando especial atención a la línea 9."
--- ejer3/apartado2.sh
+++ ejer3/apartado2.sh
@@ -15,12 +15,15 @@ valgrind --tool=cachegrind ./aos --L1=32768,8,64 --L2=262144,8,64
 cp cachegrind.out.* out_3.out
 rm -rf ./cachegrind.out.*
 
+cat *.out >> out_global.out
 cg_annotate out_1.out --auto=yes &> ../results/ejer3/ap2_1.result
 cg_annotate out_2.out --auto=yes &> ../results/ejer3/ap2_2.result
 cg_annotate out_3.out --auto=yes &> ../results/ejer3/ap2_3.result
+cg_annotate out_global.out --auto=yes &> ../results/ejer3/ap2_global.result
 cp out_1.out ../results/ejer3/ap2_1.out_valgrind
 cp out_2.out ../results/ejer3/ap2_2.out_valgrind
 cp out_3.out ../results/ejer3/ap2_3.out_valgrind
+cp out_global.out ../results/ejer3/ap2_global.out_valgrind
 
 echo "Observe los resultados obtenidos e inspeccione el código con la herramienta cg_annotate. Anote
 los resultados globales y observe los resultados prestando especial atención a las líneas 12 y 13."
